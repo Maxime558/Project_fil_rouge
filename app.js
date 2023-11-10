158f9d01a048f583ba0116ee6feaeccbea8b85a3
Maxime Lottin <maxime.lottin@outlook.com>
2023-11-10 14:54:32 +0100

    Ajout de node.js

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-import express from "express";
-import http from "http";
-const app = express();
-import { Server } from "socket.io";
+const express = require('express')
+const http = require('http')
+const app = express()
+
+const host = '127.0.0.1';
+const port = '8000';
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.sendFile('index.html',{root:__dirname})
+})
+
 const server = http.Server(app);
-const io = new Server(server);
-const host = "127.0.0.1";
-const port = "8000";
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
-});
 
-io.on("connection", (socket) => {
-  socket.on("message", (data) => {
-    console.dir(socket.id);
-    console.dir(data);
-    socket.broadcast.emit("resMessage", { data: data });
-  });
-});
-
-server.listen(port, host, () => {
-  console.log(`Server is running at http://${host}:${port}`);
-});
+server.listen(port,host,()=>{
+  console.log(`Server is running at http://${host}:${port}`)
+})
